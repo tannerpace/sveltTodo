30634eae37f558ea28f9a67669aeb048b4c62010
TannerB <tannerpace@gmail.com>
2022-06-26 18:02:28 -0400

    adds resume svg

--- my-app/src/routes/projects.svelte
+++ my-app/src/routes/projects.svelte
@@ -1,16 +1,12 @@
 <script context="module">
 	import { browser, dev } from '$app/env';
 
-	// we don't need any JS on this page, though we'll load
-	// it in dev so that we get hot module replacement...
+
 	export const hydrate = dev;
 
-	// ...but if the client-side router is already loaded
-	// (i.e. we came here from elsewhere in the app), use it
 	export const router = browser;
 
-	// since there's no dynamic data here, we can prerender
-	// it so that it gets served as a static asset in prod
+	
 	export const prerender = true;
 </script>
 
@@ -23,7 +19,7 @@
 </div>
 <div>
   <section id="projects" class="projects-section">
-    <h2 class="projects-section-header">These are some of my projects</h2>
+    <h2 class="projects-section-header">Projects</h2>
 
     <div class="projects-grid">
       <a href="https://tannerpace.github.io/crispy-couscous/" target="_blank" rel="noopener noreferrer" class="project project-tile">
