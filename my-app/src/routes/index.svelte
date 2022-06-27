8db585d5c92b9e2be1b3691fff9bf43f70115d11
TannerB <tannerpace@gmail.com>
2022-06-26 23:58:20 -0400

    idk

--- my-app/src/routes/index.svelte
+++ my-app/src/routes/index.svelte
@@ -13,7 +13,7 @@
 </svelte:head>
 
 <div class="content">
-	<h1>Hi I am <a href="https://www.tannerbleakley.com">Tanner</a></h1>
+	<h1>Hi I am <a href="tannerbleakley.com">Tanner</a></h1>
 	<p>
 		I am a software engineer based in the Charleston South Carolina area. I have
 		a passion for building software that improves the lives of people.
