8b4a9ac0aef4dc788e422c22e6efc92a22806f36
TannerB <tannerpace@gmail.com>
2022-06-26 23:34:55 -0400

    lint

--- my-app/.eslintrc.js
+++ my-app/.eslintrc.js
@@ -16,36 +16,7 @@ module.exports = {
   rules: {
     "simple-import-sort/imports": "warn",
     "simple-import-sort/exports": "warn",
-    'prettier/prettier': 'error',
-    'svelte3/no-unknown-event-handlers': 'error',
-    'svelte3/no-unknown-interfaces': 'error',
-    'svelte3/no-unknown-lifecycle-hooks': 'error',
-    'svelte3/no-unknown-slot-attributes': 'error',
-    'svelte3/no-unknown-slot-scopes': 'error',
-    'svelte3/no-unknown-slot-bindings': 'error',
-    'svelte3/no-unknown-slot-bindings-scope': 'error',
-    'svelte3/no-unknown-slot-bindings-scope-attribute': 'error',
-    'svelte3/no-unknown-slot-bindings-scope-attribute-value': 'error',
-    'svelte3/no-unknown-slot-bindings-scope-attribute-value-value': 'error',
-    'svelte3/no-unknown-slot-bindings-scope-attribute-value-value-value': 'error',
-    'svelte3/no-unknown-slot-bindings-scope-attribute-value-value-value-value': 'error',
-    'svelte3/no-unknown-slot-bindings-scope-attribute-value-value-value-value-value': 'error',
-    'svelte3/no-unknown-slot-bindings-scope-attribute-value-value-value-value-value-value': 'error',
-    'svelte3/no-unknown-slot-bindings-scope-attribute-value-value-value-value-value-value-value': 'error',
-    'svelte3/no-unknown-slot-bindings-scope-attribute-value-value-value-value-value-value-value-value': 'error',
+
   },
-  settings: {
-    'import/resolver': {
-      'babel-plugin-import': {
-        'configs': {
-          'babel-preset-svelte3': {
-            'svelte3': true
-          },
-          'babel-preset-react': {
-            'react': true
-          },
-        },
-      },
-    },
-  }
+
 }
